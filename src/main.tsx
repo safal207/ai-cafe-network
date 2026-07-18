603bb86a2e82421ca7c52313cbc717cf03a55279
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-07-18 22:06:38 +0300

    Load performance overrides after the visual system

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './brand-system.css'
+import './performance.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
